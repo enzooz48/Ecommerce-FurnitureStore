216edc071670a4d8f66ce410877d93ddbba848e1
EnZooZ <enzooz4889@gmail.com>
2022-08-28 10:23:10 +0700

    Starter Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './page/about/About';
+import Login from './page/auth/Login';
+import Register from './page/auth/Register';
+import Cart from './page/cart/Cart';
+import Checkout from './page/checkout/Checkout';
+import Detail from './page/detail/Detail';
+import Error from './page/error/Error';
+import Home from './page/home/Home';
+import Shop from './page/shop/Shop';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="/shop" element={<Shop />} />
+				<Route path="/detail/:id" element={<Detail />} />
+				<Route path="/checkout" element={<Checkout />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="*" element={<Error />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/page/checkout/Checkout.jsx
@@ -0,0 +1,7 @@
+import './Checkout.css';
+
+const Checkout = () => {
+	return <div>Checkout</div>;
+};
+
+export default Checkout;
